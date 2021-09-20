c3971f16b936b6eb349bbcd38d945733e84c21de
armelito60 <tchiasso@et.esiea.fr>
2021-09-20 16:02:09 +0200

    affichage du profil ligne du Bas Rhin

--- Prep des données et Analyse/TD2/td2.R
+++ Prep des données et Analyse/TD2/td2.R
@@ -4,10 +4,8 @@ setwd(dir = "C:/Users/Armel/Desktop/Cours ESIEA/Github/Prep des données et Analy
 president12 <- as.data.frame(read.csv2(file = "C:/Users/Armel/Desktop/Cours ESIEA/Github/Prep des données et Analyse/TD2/President12.csv", encoding = "UTF-8", row.names = 1))
 
 #Affichage les nombres de voix obtenues par les différents candidats dans le département du Bas-Rhin, Paris, Seine St Denis et Vaucluse
-barplot(as.matrix(president12[67,2:11]),names.arg=colnames(president12)[2:11],col="purple3", border="white", main="Bas-Rhin", horiz=T, las=1,xlab="nombre de voix", cex.lab=1.2)
+president_4 <- (president12[c(68,76,94,85),2:11])
+barplot(as.matrix(t(president_4)),names.arg=colnames(t(president_4)), col=rainbow(10),border="black", main="Votes", horiz=F, las=1,xlab="Nombre de voix", cex.lab=1.2, beside = TRUE, legend=TRUE, args.legend = list(x = "topright", bty = "n", inset=c(-0.005, 0)),ylim=c(0,350000))
 
-barplot(as.matrix(president12[75,2:11]),names.arg=colnames(president12)[2:11],col="purple3", border="white", main="Bas-Rhin", horiz=T, las=1,xlab="nombre de voix", cex.lab=1.2)
-
-barplot(as.matrix(president12[93,2:11]),names.arg=colnames(president12)[2:11],col="purple3", border="white", main="Bas-Rhin", horiz=T, las=1,xlab="nombre de voix", cex.lab=1.2)
-
-barplot(as.matrix(president12[84,2:11]),names.arg=colnames(president12)[2:11],col="purple3", border="white", main="Bas-Rhin", horiz=T, las=1,xlab="nombre de voix", cex.lab=1.2)
+#affichage du profil ligne du Bas-Rhin
+barplot(as.matrix(president12[68,2:11])/sum(as.matrix(president12[68,2:11])),horiz = TRUE,border="white", col="blue4", beside=T, xlab="Bas-Rhin", cex.lab=1.5, las=1)
